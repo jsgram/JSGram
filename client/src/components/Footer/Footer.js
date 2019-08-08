a7723d17b48ad3448d98658f11d3c350e7c05524
Revura Volodia <revuravova@gmail.com>
2019-08-08 19:18:53 +0300

    change Footer, .gitignore

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Footer.scss'
 
+const footerLink = [
+  { label: 'About us', href: '#'},
+  { label: 'Github', href: '#'},
+  { label: 'Demos', href: '#'},
+  { label: 'Softserve', href: '#'}
+  ]
+
 const Footer = () => (
   <div className="container-fluid text-center footer-link footer">
-    <a className="pr-3 text-danger" href="#">About us</a>
-    <a className="pr-3 text-danger" href="#">Github</a>
-    <a className="pr-3 text-danger" href="#">Demos</a>
-    <a className="pr-3 text-danger" href="#">Softserve</a>
+    {footerLink.map((footer) => (<a className="pr-3 text-danger" href={footer.href}>{footer.label}</a>))}
     <span className="pl-4">© 2019 JSgram</span>
   </div>
 )
