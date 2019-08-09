8decc369abfa0caf33ea0fa6699de400954c7e13
kalyta_ihor <kalyta.ihor@gmail.com>
2019-08-09 11:38:36 +0300

    add server side validation

--- server/src/controllers/user.controllers/create.user.ts
+++ server/src/controllers/user.controllers/create.user.ts
@@ -4,6 +4,7 @@ import {Token, ITokenModel} from '../../models/token.model';
 import nodemailer from 'nodemailer';
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
+import validateInput from '../../helpers/validation';
 
 export const create = async (req: Request,
                              res: Response,
@@ -23,63 +24,65 @@ export const create = async (req: Request,
             posts,
         }: IUserModel = req.body;
 
-        if (!email || !fullName || !username || !password) {
-            throw new Error('Some field is empty');
+        const {errors,isValid} = validateInput(req.body);
+		if(!isValid){
+            res.json(errors);          
         }
+        else{
+            const saltRounds: number = 12;
 
-        const saltRounds: number = 12;
-
-        const salt: string = bcrypt.genSaltSync(saltRounds);
-        const hash: string = bcrypt.hashSync(password, salt);
-        const emailExist = await User.countDocuments({email});
-        if (emailExist) {
-            throw new Error('The email address you have entered is ' +
-                'already associated with another account');
-        }
-
-        const createdUser: IUserModel = await User.create({
-            email,
-            fullName,
-            username,
-            password: hash,
-            dateOfBirth,
-            createdAt,
-            photoPath,
-            bio,
-            isAdmin,
-            isVerified,
-            posts,
-        });
-
-        const token: ITokenModel = await Token.create({
-            user: createdUser._id,
-            token: crypto.randomBytes(16).toString('hex'),
-        });
-
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.EMAIL_PASS,
-            },
-        });
-
-        const url = `http://localhost:8080/confirm/${token.token}`;
-
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: 'JSgram Account verification',
-            html: `<h1 style="color: red">Hello, ${fullName}, please verify your account by clicking the <a href="${url}">link</a></h1>`,
-        };
-
-        transporter.sendMail(mailOptions, (err) => {
-            if (err) {
-                console.error(err);
+            const salt: string = bcrypt.genSaltSync(saltRounds);
+            const hash: string = bcrypt.hashSync(password, salt);
+            const emailExist = await User.countDocuments({email});
+            if (emailExist) {
+                throw new Error('The email address you have entered is ' +
+                    'already associated with another account');
             }
-            res.json(
-                {status: `A verification email has been sent to ${email}`});
-        });
+    
+            const createdUser: IUserModel = await User.create({
+                email,
+                fullName,
+                username,
+                password: hash,
+                dateOfBirth,
+                createdAt,
+                photoPath,
+                bio,
+                isAdmin,
+                isVerified,
+                posts,
+            });
+    
+            const token: ITokenModel = await Token.create({
+                user: createdUser._id,
+                token: crypto.randomBytes(16).toString('hex'),
+            });
+    
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.EMAIL_PASS,
+                },
+            });
+    
+            const url = `http://localhost:8080/confirm/${token.token}`;
+    
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: email,
+                subject: 'JSgram Account verification',
+                html: `<h1 style="color: red">Hello, ${fullName}, please verify your account by clicking the <a href="${url}">link</a></h1>`,
+            };
+    
+            transporter.sendMail(mailOptions, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                res.json(
+                    {status: `A verification email has been sent to ${email}`});
+            });
+        }
 
     } catch (e) {
         next(e);
