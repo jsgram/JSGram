a7f3123bdd836fe551aa48695acbc374f12fac62
Rostyslav <rostyk1331@gmail.com>
2019-08-10 18:41:59 +0300

    refactoring

--- client/src/components/Login/LoginContainer.tsx
+++ client/src/components/Login/LoginContainer.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import Login from "./Login";
 import { reduxForm } from "redux-form";
 import API from "../../store/api";
-import { showAlert } from "../../store/alert/actions";
 
 const TOKEN = "TOKEN";
 
 class LoginContainer extends React.Component<any> {
-  onSubmit = (user: any, dispatch: Function) => {
+  onSubmit = (user: any) => {
     return API.post("/auth/login", user)
       .then(response => {
         localStorage.setItem(TOKEN, response.data.token);
-        dispatch(showAlert(response.data.status, "success"));
       })
       .catch(err => console.log(err));
   };
