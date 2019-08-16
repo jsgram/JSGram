f785af08b5758ae7af5d8165f690a7b9130c9866
Rostyslav <rostyk1331@gmail.com>
2019-08-16 23:22:08 +0300

    small refactoring

--- server/src/controllers/profile.controllers/getProfile.ts
+++ server/src/controllers/profile.controllers/getProfile.ts
@@ -7,20 +7,20 @@ export const getProfile = async (req: Request, res: Response, next: NextFunction
         const fakeUser = {
             posts: ['post1', 'post2', 'post3'],
             followers: ['follower1', 'follower2', 'follower3'],
-            subscribers: ['subscriber1', 'subscriber2'],
+            following: ['subscriber1', 'subscriber2'],
             description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
                           'Harum iusto nesciunt repudiandae vel! Accusamus aliquid animi consequatur,' +
                           'consequuntur cumque dolorem eum eveniet fugit iste labore magni,' +
                           'obcaecati perferendis rerum veniam!',
         };
-        const { posts, followers, subscribers, description }: any = fakeUser;
+        const { posts, followers, following, description }: any = fakeUser;
 
         const user = await findById(req.params.id, next);
 
         const userProfile = {
             posts: posts.length,
             followers: followers.length,
-            subscribers: subscribers.length,
+            following: following.length,
             description,
             fullName: user.fullName,
             photo: user.photoPath,
