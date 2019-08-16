a7574bdd9230397271a15ab3c1b505158586a6e5
mariasykpotter <mpetrivska21@gmail.com>
2019-08-16 21:22:49 +0300

    first

---
+++ client/src/components/Menu/Menu.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import MainScreen from './Components/MainScreen'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab} from '@fortawesome/free-brands-svg-icons'
+import {faHeart,faCompass,faSearch,faUser} from '@fortawesome/free-solid-svg-icons'
+import './App.css'
+ 
+library.add(fab, faHeart,faCompass,faSearch,faUser)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <nav className='navbar'>
+        <div className="logo">
+        <FontAwesomeIcon className="inst" icon={['fab','instagram']} size="lg"/>
+        <h1>JSGram</h1>
+        </div>
+        <div className="center">
+          <FontAwesomeIcon className="site-search" icon={faSearch} size="xs"/>
+        <input type="search" className="input" name="q" placeholder="Search"></input>
+        </div>
+        <ul className="list">
+          <li><FontAwesomeIcon className="compass" icon={faCompass} size="lg"/></li>
+          <li><FontAwesomeIcon className="heart" icon={faHeart}  size="lg"/></li>
+          <li><FontAwesomeIcon className="user" icon={faUser} size="lg"/></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
