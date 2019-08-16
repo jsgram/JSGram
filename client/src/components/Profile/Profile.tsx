ec7e1b7c66230a8ac7fe05fccfeba22914c45467
Revura Volodia <revuravova@gmail.com>
2019-08-16 17:02:23 +0300

    fix mistakes, delete unnecessary classes

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -18,7 +18,7 @@ interface IFormProps {
 
 export default class Profile extends React.Component<IFormProps> {
 
-    public state: {loaded: boolean} = {
+    public state: { loaded: boolean } = {
         loaded: false,
     };
     public timerHandle: any = 0;
@@ -46,46 +46,44 @@ export default class Profile extends React.Component<IFormProps> {
 
     public render(): JSX.Element {
         const {user: {first_name, last_name, avatar}}: any = this.props;
-        const {loaded}: {loaded: boolean} = this.state;
+        const {loaded}: { loaded: boolean } = this.state;
 
         if (!loaded) {
             return (<Instagram/>);
         }
         return (
-                <div className='profile-header'>
-                    <div className='container'>
-                        <div className='row'>
-                            <div className='profile d-flex mt-5'>
-                                <div className='col-md-4 col-sm-4 col-4 mr-md-4 mr-sm-4'>
-                                    <img
-                                        src={avatar}
-                                        className='img-fluid rounded-circle'/>
-                                </div>
-                                <div>
-                                    <p className='profile-name'>
-                                        {first_name} {last_name}
-                                        <Button className='bg-dark ml-md-2 ml-sm-2 ml-2 btn'>
-                                            Edit profile
-                                        </Button>
-                                    </p>
-                                    <div className='d-flex followers'>
-                                        <div>
-                                            <a href='#' className='mr-md-4 mr-sm-4 mr-3'>Posts</a>
-                                            <p>1000</p>
-                                        </div>
-                                        <div>
-                                            <a href='#' className='mr-md-4 mr-sm-4 mr-3'>Followers</a>
-                                            <p className='ml-3'>50m</p>
-                                        </div>
-                                        <div>
-                                            <a href='#'>Following</a>
-                                            <p className='ml-4'>12</p>
-                                        </div>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='profile d-flex mt-5'>
+                            <div className='col-4 mr-sm-4'>
+                                <img
+                                    src={avatar}
+                                    className='img-fluid rounded-circle'/>
+                            </div>
+                            <div>
+                                <p className='profile-name'>
+                                    {first_name} {last_name}
+                                    <Button className='bg-dark ml-2 btn'>
+                                        Edit profile
+                                    </Button>
+                                </p>
+                                <div className='d-flex followers'>
+                                    <div>
+                                        <a href='#' className='mr-sm-4 mr-3'>Posts</a>
+                                        <p>1000</p>
                                     </div>
-                                    <Button className='btn' color='danger'><i
-                                        className='fa fa-plus pr-3'/>Add
-                                        Post</Button>
+                                    <div>
+                                        <a href='#' className='mr-sm-4 mr-3'>Followers</a>
+                                        <p className='ml-3'>50m</p>
+                                    </div>
+                                    <div>
+                                        <a href='#'>Following</a>
+                                        <p className='ml-4'>12</p>
+                                    </div>
                                 </div>
+                                <Button className='btn' color='danger'><i
+                                    className='fa fa-plus pr-3'/>Add
+                                    Post</Button>
                             </div>
                         </div>
                     </div>
