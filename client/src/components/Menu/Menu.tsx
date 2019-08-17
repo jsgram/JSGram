ef89eb2087edc6d0cbfbda35aa3bfb97871addb9
mariasykpotter <mpetrivska21@gmail.com>
2019-08-17 21:19:59 +0300

    response to comments

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -1,31 +1,19 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab} from '@fortawesome/free-brands-svg-icons';
-import {faHeart, faCompass, faSearch, faUser} from '@fortawesome/free-solid-svg-icons';
+import React, {ReactElement} from 'react';
 import '../styles/Menu.scss';
-
-library.add(fab, faHeart, faCompass, faSearch, faUser);
-class Menu extends React.Component {
-    public render():
-    JSX.Element {
-        return (
+export const Menu: React.FC = (): ReactElement => (
       <nav className='navig'>
         <div className='logo_component'>
-        <FontAwesomeIcon className='inst_icon' icon={['fab', 'instagram']} size='lg'/>
+        <i className='fa-2x fa fa-instagram inst_icon'></i>
         <h1 className='jsgram'>JSGram</h1>
         </div>
         <div className='center-component'>
-          <FontAwesomeIcon className='find_icon' icon={faSearch} size='xs'/>
+        <i className='fa-xxs fa fa-search find_icon'></i>
         <input type='search' className='input_input' name='q' placeholder='Search'></input>
         </div>
         <ul className='list'>
-          <li className='li_component'><FontAwesomeIcon className='compass_icon' icon={faCompass} size='lg'/></li>
-          <li className='li_component'><FontAwesomeIcon className='heart_icon' icon={faHeart}  size='lg'/></li>
-          <li className='li_component'><FontAwesomeIcon className='user_icon' icon={faUser} size='lg'/></li>
+          <li className='li_component'><i className='fa-2x fa fa-compass compass_icon'></i></li>
+          <li className='li_component'><i className='fa-2x fa fa-heart heart_icon'></i></li>
+          <li className='li_component'><i className='fa-2x fa fa-user user_icon'></i></li>
         </ul>
       </nav>
-        );
-    }
-}
-export default Menu;
+);
