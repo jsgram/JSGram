e7db27916dcef86e590da5aa1933c9c8f8ea3f6f
mariasykpotter <mpetrivska21@gmail.com>
2019-08-18 12:37:15 +0300

    took comments into account

--- client/src/components/Menu/Menu.tsx
+++ client/src/components/Menu/Menu.tsx
@@ -6,14 +6,14 @@ export const Menu: React.FC = (): ReactElement => (
         <i className='fa-2x fa fa-instagram inst_icon'></i>
         <h1 className='jsgram'>JSGram</h1>
         </div>
-        <div className='center-component'>
+        <div className='center_part'>
         <i className='fa-xxs fa fa-search find_icon'></i>
         <input type='search' className='input_input' name='q' placeholder='Search'></input>
         </div>
-        <ul className='list'>
-          <li className='li_component'><i className='fa-2x fa fa-compass compass_icon'></i></li>
-          <li className='li_component'><i className='fa-2x fa fa-heart heart_icon'></i></li>
-          <li className='li_component'><i className='fa-2x fa fa-user user_icon'></i></li>
+        <ul className='icons_list'>
+          <li className='li_component'><i className='fa-2x fa fa-compass'></i></li>
+          <li className='li_component'><i className='fa-2x fa fa-heart'></i></li>
+          <li className='li_component'><i className='fa-2x fa fa-user'></i></li>
         </ul>
       </nav>
 );
