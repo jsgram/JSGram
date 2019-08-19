6254222f8f22bef77c22b6e8e5dfc2dce44b5b9d
Rostyslav <rostyk1331@gmail.com>
2019-08-19 09:10:17 +0300

    small fixes when no token

--- server/src/controllers/auth.controllers/auth.isAuthorized.ts
+++ server/src/controllers/auth.controllers/auth.isAuthorized.ts
@@ -5,7 +5,7 @@ export const isAuthorized = async (req: Request, res: Response, next: NextFuncti
     try {
         const token = req.get('x-access-token');
         if (!token) {
-            return res.status(401).send({errors: [{title: 'Unauthorized', detail: 'Invalid token'}]})
+            return res.status(401).send({errors: [{title: 'Unauthorized', detail: 'No token'}]})
                       .redirect(`${process.env.FRONT_PATH}/`);
         }
         const user = await tokenVerification(token, res, next);
