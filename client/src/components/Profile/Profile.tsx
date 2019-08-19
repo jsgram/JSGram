d6155da5c550466ac1da1326903a7ecf3131d0ce
mariasykpotter <mpetrivska21@gmail.com>
2019-08-19 23:38:34 +0300

    fixed linter

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import { Button } from 'reactstrap';
 import '../styles/Profile.scss';
 import Cropper from '../Cropper/Cropper';
 import noAvatar from '../assets/noAvatar.svg';
-import { Menu } from '../Menu/Menu'
+import { Menu } from '../Menu/Menu';
 
 export interface IUserData {
     posts: number;
