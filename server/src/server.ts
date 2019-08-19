0f30441499e566d8f82e87b634b0659f788d235c
Revura Volodia <revuravova@gmail.com>
2019-08-19 14:29:20 +0300

    fix bug alert and add some style for profile

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import {app} from './app';
 import connect from './connect';
 
-app.listen(process.env.DEV_PORT, () => console.info('Listening...');
+app.listen(process.env.DEV_PORT, () => console.info('Listening...'));
 connect(process.env.DB_PATH);
