7b8d7e0893d5d3c6909283b0eff2d4152e2f10ce
Yurii Martynenko <ymartinenko4@gmail.com>
2019-08-20 23:49:15 +0300

    update expiresIn

--- server/src/helpers/jwt.encoders.ts
+++ server/src/helpers/jwt.encoders.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const encodeJWT = (email: string, secret: string): string => {
-    return jwt.sign({email}, secret, {expiresIn: '30s'});
+    return jwt.sign({email}, secret, {expiresIn: '24h'});
 };
 
 export const decodeJWT = (token: string, secret: string): string | object => {
