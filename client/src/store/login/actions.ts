5606aa8fe8410a5d9d78626f3906cd9f59e2d1cc
IhorKalyta <kalyta.ihor@gmail.com>
2019-08-20 11:16:40 +0300

    'Autorization'

--- client/src/store/login/actions.ts
+++ client/src/store/login/actions.ts
@@ -3,6 +3,23 @@ import { showAlert } from '../alert/actions';
 import { Dispatch } from 'redux';
 import { IUser } from '../commonInterfaces/commonInterfaces';
 import {setToken} from './setToken.helper';
+import {GET_USER_PENDING, GET_USER_SUCCESS, GET_USER_ERROR} from '../profile/actionTypes';
+import {IUserData} from '../../components/Profile/Profile';
+import {TOKEN} from '../login/setToken.helper';
+
+export const getUserPending = (): { type: string } => ({
+    type: GET_USER_PENDING,
+});
+
+export const getUserSuccess = (user: IUserData): { type: string, payload: any } => ({
+    type: GET_USER_SUCCESS,
+    payload: user,
+});
+
+export const getUserError = (error: Error): { type: string, payload: Error } => ({
+    type: GET_USER_ERROR,
+    payload: error,
+});
 
 export const loginUser = (user: IUser): (dispatch: Dispatch) => Promise<void> =>
     async (dispatch: Dispatch): Promise<void> => {
@@ -12,6 +29,13 @@ export const loginUser = (user: IUser): (dispatch: Dispatch) => Promise<void> =>
                 dispatch(showAlert('Welcome', 'success'));
             }
             setToken(res.data.token);
+            dispatch(getUserPending());
+            const token = localStorage.getItem(TOKEN);
+            const get = await API.get(('/'), {
+                headers: { Authorization: token },
+            });
+
+            dispatch(getUserSuccess(get.data));
         } catch (e) {
             dispatch(showAlert(e.response.data.message, 'danger'));
         }
