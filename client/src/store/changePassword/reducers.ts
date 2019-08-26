8fc0a59e1a09e249dbfe250f259439d8727da793
kalyta <kalyta.ihor@gmail.com>
2019-08-26 21:35:34 +0300

    reduxChangePassword

---
+++ client/src/store/changePassword/reducers.ts
@@ -0,0 +1,40 @@
+import { CHANGE_PASSWORD_PENDING, CHANGE_PASSWORD_SUCCESS, CHANGE_PASSWORD_ERROR } from './actionTypes';
+
+interface IAction {
+    type: string;
+    payload: any;
+}
+
+export const defaultState = {
+    password : '',
+    error: '',
+    loaded: false,
+    loading: false,
+};
+
+export const changePasswordReducer = (
+        state: {password: string} = defaultState,
+        action: IAction): any => {
+    switch (action.type) {
+    case CHANGE_PASSWORD_PENDING:
+        return {
+            ...state,
+            loaded: false,
+            error: '',
+        };
+    case CHANGE_PASSWORD_SUCCESS:
+        return {
+            ...state,
+            password: action.payload,
+            loaded: true,
+        };
+    case CHANGE_PASSWORD_ERROR:
+        return {
+            ...state,
+            error: action.payload,
+            loaded: false,
+        };
+    default:
+        return state;
+    }
+};
