574fc4dd5690ce23618b493d81c32e483b4999d9
kalyta <kalyta.ihor@gmail.com>
2019-09-06 11:51:12 +0300

    spinner for uploading avatar

--- client/src/store/cropper/actions.ts
+++ client/src/store/cropper/actions.ts
@@ -1,32 +1,13 @@
 import {
     SET_AVATAR_TO_CROPPER,
-    UPLOAD_AVATAR_PENDING,
-    UPLOAD_AVATAR_SUCCESS,
-    UPLOAD_AVATAR_ERROR,
 } from './actionTypes';
 import { Dispatch } from 'redux';
-import { AuthAPI } from '../api';
 import { showAlert } from '../alert/actions';
-import { setPhotoToState } from '../profile/actions';
-import { base64ToFile, createDataForAWS } from '../../helpers/upload.photo';
+import { base64ToFile } from '../../helpers/upload.photo';
 
 export const setAvatarToCropper = (avatar: any): { type: string, payload: File } => ({
     type: SET_AVATAR_TO_CROPPER,
     payload: avatar,
-});
-
-export const uploadAvatarPending = (): { type: string } => ({
-    type: UPLOAD_AVATAR_PENDING,
-});
-
-export const uploadAvatarSuccess = (avatar: any): { type: string, payload: File } => ({
-    type: UPLOAD_AVATAR_SUCCESS,
-    payload: avatar,
-});
-
-export const uploadAvatarError = (error: Error): { type: string, payload: Error } => ({
-    type: UPLOAD_AVATAR_ERROR,
-    payload: error,
 });
 
 export const informFileIsTooBig = (): (dispatch: Dispatch) => void =>
@@ -41,19 +22,5 @@ export const createFile = (preview: string): (dispatch: Dispatch) => Promise<voi
             dispatch(setAvatarToCropper(newFile));
         } catch (e) {
             dispatch(showAlert(e.response.data.message, 'danger'));
-        }
-    };
-
-export const uploadPostAvatar = (avatar: File): (dispatch: Dispatch) => Promise<void> =>
-    async (dispatch: Dispatch): Promise<void> => {
-        try {
-            dispatch(uploadAvatarPending());
-            const res = await AuthAPI.post('/profile/photo', createDataForAWS('userPhoto', avatar));
-            dispatch(showAlert('Successfully uploaded', 'success'));
-            dispatch(uploadAvatarSuccess(res.data.userProfile));
-            dispatch(setPhotoToState(res.data.photoPath));
-        } catch (e) {
-            dispatch(showAlert(e.response.data.message, 'danger'));
-            dispatch(uploadAvatarError(e.response.data));
         }
     };
