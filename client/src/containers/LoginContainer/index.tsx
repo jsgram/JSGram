5c9ec4abf0c55ab525ffb1864ab5c5f4466bcffe
didostap <dribnucko@gmail.com>
2019-09-07 11:16:53 +0300

    fix username navigation

--- client/src/containers/LoginContainer/index.tsx
+++ client/src/containers/LoginContainer/index.tsx
@@ -6,21 +6,31 @@ import { loginUser } from '../../store/login/actions';
 import validate from '../../utils/validation';
 import { FormProps } from 'reactstrap';
 import { IUser } from '../../store/commonInterfaces/commonInterfaces';
+import { setToken } from '../../store/login/setToken.helper';
+import { history } from '../../history';
 
-export const LoginContainer = (props: any): JSX.Element => {
-    const onSubmit = (user: IUser): void => {
-        return props.loginUser(user);
-    };
+class LoginContainer extends React.Component<any> {
+    public componentWillMount(): void {
+        const {token}: { token: string } = this.props.match.params;
+        if (token) {
+            setToken(token);
+            history.push('/feed');
+        }
+    }
 
-    const {handleSubmit, submitting}: FormProps = props;
-    return (
-        <Login
-            handleSubmit={handleSubmit}
-            onSubmit={onSubmit}
-            submitting={submitting}
-        />
-    );
-};
+    public onSubmit = (user: IUser): void => this.props.loginUser(user);
+
+    public render(): JSX.Element {
+        const {handleSubmit, submitting}: FormProps = this.props;
+        return (
+            <Login
+                handleSubmit={handleSubmit}
+                onSubmit={this.onSubmit}
+                submitting={submitting}
+            />
+        );
+    }
+}
 
 export default connect(
     null,
