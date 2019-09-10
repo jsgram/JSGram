ceb9c3506e26b4b1f5f6cd341aa273f4d734a7bd
Rostyslav <51924858+rostyk1331@users.noreply.github.com>
2019-09-10 12:05:32 +0300

    Update get.user.ts

--- server/src/controllers/main.controllers/get.user.ts
+++ server/src/controllers/main.controllers/get.user.ts
@@ -2,9 +2,10 @@ import { Request, Response, NextFunction } from 'express';
 
 export const getUser = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const { locals: { user: { username, email } } }: Response = res;
+        const { locals: { user: { _id, username, email } } }: Response = res;
 
         res.json({
+            _id,
             username,
             email,
         });
