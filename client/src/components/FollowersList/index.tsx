e11f5318265ea97544fe4b44fa5d1d036b8aba1f
mariasykpotter <mpetrivska21@gmail.com>
2019-09-12 20:54:51 +0300

    not modal anymore:(

---
+++ client/src/components/FollowersList/index.tsx
@@ -0,0 +1,33 @@
+import React, { ReactElement } from 'react';
+import { Button } from 'reactstrap';
+import userphoto from '../../assets/user1.png';
+import './style.scss';
+interface IFollowData {
+    username: string;
+}
+export const FollowList = ({title,data}: { title: string, data: IFollowData[]}): ReactElement=> (
+    <div>
+        <h4 className='text-center'>{title}</h4>
+            <div className='d-flex justify-content-center'> 
+                    <div>
+                    {data.map((item: any) => (
+                        <div className='d-flex mt-1 mb-3 justify-content-between' key={item.username}>
+                            <div className='row'>
+                                <img
+                                    src={userphoto}
+                                    alt='avatar'
+                                    width={32}
+                                    height={32}
+                                    className='img-fluid rounded-circle ml-3 mr-2 mt-1'
+                                />
+                                <h6 className='align-self-end'>{item.username}</h6>
+                            </div>
+                            <div className='followers-list'>
+                            </div>
+                            <Button color='danger'>Follow</Button>
+                        </div>
+                    ))}
+                    </div>
+            </div>
+        </div>
+        );
