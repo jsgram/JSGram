820ce702a19aef5d0b542658d45d2442abbf03de
mariasykpotter <mpetrivska21@gmail.com>
2019-09-12 23:02:05 +0300

    fixed empty div

--- client/src/components/FollowersList/index.tsx
+++ client/src/components/FollowersList/index.tsx
@@ -9,7 +9,7 @@ export const FollowList = ({title,data}: { title: string, data: IFollowData[]}):
     <div>
         <h4 className='text-center'>{title}</h4>
             <div className='d-flex justify-content-center'> 
-                    <div>
+                    <div className='follow-wrapper'>
                     {data.map((item: any) => (
                         <div className='d-flex mt-1 mb-3 justify-content-between' key={item.username}>
                             <div className='row'>
@@ -21,8 +21,6 @@ export const FollowList = ({title,data}: { title: string, data: IFollowData[]}):
                                     className='img-fluid rounded-circle ml-3 mr-2 mt-1'
                                 />
                                 <h6 className='align-self-end'>{item.username}</h6>
-                            </div>
-                            <div className='followers-list'>
                             </div>
                             <Button color='danger'>Follow</Button>
                         </div>
