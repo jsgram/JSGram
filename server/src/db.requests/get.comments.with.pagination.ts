e638fe4c1aee32029879f6047e7807f84d7be43b
zennarchi <art256100@gmail.com>
2019-09-13 15:49:39 +0300

    Add populate for comments

--- server/src/db.requests/get.comments.with.pagination.ts
+++ server/src/db.requests/get.comments.with.pagination.ts
@@ -5,7 +5,11 @@ import { COMMENTS_PER_PAGE } from '../controllers/comments.controllers/get.comme
 export const getCommentsWithPagination = async (postId: string, skip: number, next: NextFunction):
     Promise<ICommentModel[] | void | null> => {
     try {
-        return await Comment.find({postId}).limit(COMMENTS_PER_PAGE).skip(skip);
+        return await Comment
+            .find({postId})
+            .populate('authorId', ['username', '_id', 'photoPath'])
+            .limit(COMMENTS_PER_PAGE)
+            .skip(skip);
     } catch (e) {
         next({status: 409, message: 'Error with fetching comments'});
     }
