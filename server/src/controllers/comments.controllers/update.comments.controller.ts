2d77cb3423c374e1b035ad57ab35e68b72f0458a
zennarchi <art256100@gmail.com>
2019-09-15 14:09:42 +0300

    Fix destruction

--- server/src/controllers/comments.controllers/update.comments.controller.ts
+++ server/src/controllers/comments.controllers/update.comments.controller.ts
@@ -1,10 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
 import { updateComment } from '../../db.requests/update.comment.request';
 
+interface IParams {
+    params: {
+        id: string;
+    };
+}
+
+interface IReqBody {
+    body: {
+        comment: string;
+    };
+}
+
 export const update = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-        const {id}: {id: string} = req.params;
-        const {comment}: {comment: string} = req.body;
+        const { params: { id } }: IParams = req;
+        const { body: { comment } }: IReqBody = req;
 
         const updatedComment = await updateComment(id, comment, next);
 
