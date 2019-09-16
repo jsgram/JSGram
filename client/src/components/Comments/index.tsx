cfebbef7ffce8c7f968262635d310afc281d9f12
zennarchi <art256100@gmail.com>
2019-09-16 14:42:13 +0300

    Fix the edit for the owner of comment

--- client/src/components/Comments/index.tsx
+++ client/src/components/Comments/index.tsx
@@ -80,53 +80,49 @@ class Comments extends React.Component<ICommentsProps> {
                                     <span className='mt-1'>{comment.authorId.username}</span>
                                     {
                                         comment.isEdit ?
-                                            (
-                                                <>
-                                                     <textarea
-                                                         rows={3}
-                                                         className='form-control'
-                                                         value={comment.newComment || comment.comment}
-                                                         onChange={
-                                                             (event: React.ChangeEvent<any>)
-                                                                 : void => this.props.changeComment(
-                                                                 event.target.value,
-                                                                 comment._id,
-                                                             )
-                                                         }
-                                                     />
-                                                    <div className='btn btn-danger mt-2'
-                                                         onClick={(): void => this.editComment(
-                                                             comment.newComment,
+                                            <>
+                                                 <textarea
+                                                     rows={3}
+                                                     className='form-control'
+                                                     value={comment.newComment || comment.comment}
+                                                     onChange={
+                                                         (event: React.ChangeEvent<any>)
+                                                             : void => this.props.changeComment(
+                                                             event.target.value,
                                                              comment._id,
-                                                             comment.authorId.email,
-                                                         )}
-                                                    >
-                                                        Change
-                                                    </div>
-                                                    <div className='btn btn-danger mt-2 ml-2'
-                                                         onClick={(): void => this.props.changeEditStatus(
-                                                             comment._id,
-                                                         )}
-                                                    >
-                                                        Cancel
-                                                    </div>
-                                                </>
-                                            )
+                                                         )
+                                                     }
+                                                 />
+                                                <div className='btn btn-danger mt-2'
+                                                     onClick={(): void => this.editComment(
+                                                         comment.newComment,
+                                                         comment._id,
+                                                         comment.authorId.email,
+                                                     )}
+                                                >
+                                                    Change
+                                                </div>
+                                                <div className='btn btn-danger mt-2 ml-2'
+                                                     onClick={(): void => this.props.changeEditStatus(
+                                                         comment._id,
+                                                     )}
+                                                >
+                                                    Cancel
+                                                </div>
+                                            </>
                                             :
-                                            (
-                                                <div className='d-inline-flex mt-3 float-right
-                                                        edit-delete-comment'>
+                                            this.props.user.email === comment.authorId.email &&
+                                            <>
+                                                <div className='d-inline-flex mt-3 float-right edit-delete-comment'>
                                                     <i
-                                                        className='fa fa-pencil mr-2 edit-comment'
-                                                        onClick={(): void => this.props.changeEditStatus(
-                                                            comment._id,
-                                                        )}
+                                                    className='fa fa-pencil mr-2 edit-comment'
+                                                    onClick={(): void => this.props.changeEditStatus(comment._id)}
                                                     />
                                                     <i className='fa fa-trash-o delete-comment'/>
                                                 </div>
-                                            )
+                                                <p>{comment.comment}</p>
+                                            </>
                                     }
-                                    <p>{comment.comment}</p>
                                 </div>
                             ),
                         )
