92ff8aa14dee09c891514d9a633e7af94b089b4c
Rostyslav <rostyk1331@gmail.com>
2019-09-18 00:18:59 +0300

    add title

--- client/src/components/FriendsRecommendationsList/index.tsx
+++ client/src/components/FriendsRecommendationsList/index.tsx
@@ -28,6 +28,7 @@ class FriendsRecommendationsList extends React.Component <IProps> {
                 <Menu/>
                 <div className='d-flex justify-content-center'>
                     <div className='follow-wrapper'>
+                        <h4 className='text-center'>Recommendations:</h4>
                         {users.map((user: IUser) =>
                             <div className='d-flex mt-1 mb-3 justify-content-between' key={user._id}>
                                 <div className='row'>
