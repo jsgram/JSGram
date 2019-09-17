2431aad7716880c0f4c63fa9c3082b2e03f7b361
zennarchi <art256100@gmail.com>
2019-09-17 23:07:24 +0300

    Fix on FE and BE

--- server/src/db.requests/deletePost.request.ts
+++ server/src/db.requests/deletePost.request.ts
@@ -4,10 +4,11 @@ import { NextFunction } from 'express';
 
 export const deletePost = async (postId: string, userId: string, next: NextFunction): Promise<any> => {
     try {
-        const delPost = await Post.findByIdAndDelete(postId);
-        if (!delPost) {
+        const delPost = await Post.findById(postId);
+        if (!delPost || delPost.author.toString() !== userId.toString()) {
             throw new Error('Post doesn\'t exist');
         }
+        delPost.remove();
 
         await User.findByIdAndUpdate(userId, { $pull: {posts: postId} });
 
