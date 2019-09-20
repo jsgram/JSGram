633bfc14f893ab66a4d8d2f20a14c44ae0903fdf
kalyta <kalyta.ihor@gmail.com>
2019-09-20 21:47:57 +0300

    BE BL Search users

--- server/src/db.requests/search.request.ts
+++ server/src/db.requests/search.request.ts
@@ -11,7 +11,7 @@ export const findUsers = async (query: string, skip: number, next: NextFunction)
             .skip(skip);
 
         if (!users) {
-            throw new Error('Can not find users');
+            throw new Error('Can not find user');
         }
 
         return users;
