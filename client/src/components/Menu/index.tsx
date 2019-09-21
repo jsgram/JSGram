219d4e8d3e0ab57cf9f4b5beb8fd6f2302ac4d6d
Volodymyr <revuravova@gmail.com>
2019-09-21 19:01:34 +0300

    ui for search users

--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -13,15 +13,17 @@ import { IStateProfileEdit } from '../../store/profileEdit/reducers';
 import noAvatar from '../../assets/noAvatar.png';
 
 const users = [
-    {id: 1, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz5FG9tU' +
-            'w40WJMHOTvhYmJqXhu0mAOjjP_4dzS7Z3bULrLICZL', username: 'volodiarevura'},
-    {id: 2, image: noAvatar, username: 'didOstap'},
-    {id: 3, image: noAvatar, username: 'artem25'},
-    {id: 4, image: noAvatar, username: 'yuriimartinenko'},
-    {id: 5, image: noAvatar, username: 'oleksiydorozhkin'},
-    {id: 6, image: noAvatar, username: 'rostyslav'},
-    {id: 7, image: noAvatar, username: 'ihorkalyta'},
-    {id: 8, image: noAvatar, username: 'marianna'},
+    {
+        id: 1, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz5FG9tU' +
+            'w40WJMHOTvhYmJqXhu0mAOjjP_4dzS7Z3bULrLICZL', username: 'volodiarevura', fullname: 'Volodia Revura',
+    },
+    {id: 2, image: noAvatar, username: 'didOstap', fullname: 'Ostap Dribniuk'},
+    {id: 3, image: noAvatar, username: 'artem25', fullname: 'Artem Pavliuk'},
+    {id: 4, image: noAvatar, username: 'yuriimartinenko', fullname: 'Yurii Martinenko'},
+    {id: 5, image: noAvatar, username: 'oleksiydorozhkin', fullname: 'Oleksiy Dorozhkin'},
+    {id: 6, image: noAvatar, username: 'rostyslav', fullname: 'Rostyslva Khanas'},
+    {id: 7, image: noAvatar, username: 'ihorkalyta', fullname: 'Ihor Kalyta'},
+    {id: 8, image: noAvatar, username: 'marianna', fullname: 'Marianna Petrivska'},
 ]
 
 interface IStateFeed {
@@ -43,10 +45,16 @@ class Menu extends React.Component<IMenuProps> {
         isMenuOpen: false,
     };
 
-    public toggle = (): void => {
-        this.setState({
-            isMenuOpen: true,
-        });
+    public toggle = (e: any): void => {
+        if (e.target.value.trim() !== '') {
+            this.setState({
+                isMenuOpen: true,
+            });
+        } else {
+            this.setState({
+                isMenuOpen: false,
+            });
+        }
     }
 
     public render(): JSX.Element {
@@ -54,13 +62,13 @@ class Menu extends React.Component<IMenuProps> {
         return (
             <div className='container-fluid header-menu'>
                 <div className='row justify-content-between bg-white'>
-                    <Link to='/' className='d-sm-block d-none'>
+                    <Link to='/'>
                         <img src={logo}
                              alt='logo'
                              width={110}
                              className='mb-3 sm-mb-2 sm-mr-5 pt-1 logo'/>
                     </Link>
-                    <div className='col-sm-4 col-12 mt-4 form-group search'>
+                    <div className='col-sm-4 col-4 mt-4 form-group search'>
                         <span className='fa fa-search form-control-feedback'/>
                         <Input
                             placeholder='Search'
@@ -68,30 +76,34 @@ class Menu extends React.Component<IMenuProps> {
                             className='form-control px-4'
                             onChange={this.toggle}
                         />
-                        <Dropdown isOpen={this.state.isMenuOpen} color='light'>
+                        <Dropdown isOpen={this.state.isMenuOpen} color='light' className='search-menu'>
                             <DropdownToggle tag='a' className='nav-link m-0 p-0'/>
                             <DropdownMenu className='scrollable-menu col-12'>
                                 {users.map((user: any) => (
-                                    <Link to={`/profile/feed`} className='text-decoration-none'>
-                                        <div className='w-100'>
-                                            <DropdownItem key={user.id} className='p-md-2 p-1'>
-                                                <img
-                                                    src={user.image}
-                                                    width={32}
-                                                    height={32}
-                                                    className='rounded-circle mr-2'
-                                                />
-                                                <span>{user.username}</span>
-                                            </DropdownItem>
-                                            <DropdownItem divider/>
-                                        </div>
-                                    </Link>
+                                        <Link to={`/profile/feed`} className='text-decoration-none'>
+                                            <div className='w-100'>
+                                                <DropdownItem key={user.id} className='p-md-2 p-1'>
+                                                    <img
+                                                        src={user.image}
+                                                        width={32}
+                                                        height={32}
+                                                        className='rounded-circle mr-2'
+                                                        alt='avatar'
+                                                    />
+                                                    <span className='font-weight-bold'>{user.username}<br/></span>
+                                                    <span className='ml-4 pl-3 fullname'>
+                                                    {user.fullname}
+                                                </span>
+                                                </DropdownItem>
+                                                <DropdownItem divider/>
+                                            </div>
+                                        </Link>
                                     ),
                                 )}
                             </DropdownMenu>
                         </Dropdown>
                     </div>
-                    <div className='mt-4 d-sm-block d-none'>
+                    <div className='mt-4'>
                         <i className='fa fa-compass fa-lg pt-2 pb-2 pr-4 sm-pt-0 icon text-muted'/>
                         <i className='fa fa-heart-o fa-lg pr-4 icon text-muted'/>
                         <Link to={`/profile/${newUsername ? newUsername : loggedUsername}`}>
