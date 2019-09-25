b299bdc9c6f81bdbabf469a1a5a849c9ce771ef2
Volodymyr <revuravova@gmail.com>
2019-09-25 12:06:06 +0300

    fix aws

--- server/src/common.constants/aws.multer.profile.constants.ts
+++ server/src/common.constants/aws.multer.profile.constants.ts
@@ -1,5 +1,5 @@
 export const acl: string = 'public-read';
-export const bucket: string = 'jsgram-profile-images';
+export const bucket: string = 'jsgram-profile-images1';
 export const secretAccessKey: string = process.env.AWS_SECRET_ACCESS_KEY as string;
 export const accessKeyId: string = process.env.AWS_ACCESS_KEY_ID as string;
 export const region: string = process.env.AWS_REGION as string;
