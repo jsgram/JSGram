69a6a461f4d6ce3df4f792782c1503ada06210c5
kalyta <kalyta.ihor@gmail.com>
2019-09-25 20:33:51 +0300

    add escapeRegExp function

--- server/src/db.requests/search.request.ts
+++ server/src/db.requests/search.request.ts
@@ -2,10 +2,14 @@ import { NextFunction } from 'express';
 import { User } from '../models/user.model';
 import { SEARCH_INFO_PER_PAGE } from '../common.constants/getPosts.constants';
 
+const escapeRegExp = (text: string): string => {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+};
+
 export const findUsers = async (query: string, skip: number, next: NextFunction):
     Promise<any> => {
     try {
-        const regex = new RegExp(query, 'i');
+        const regex = new RegExp(escapeRegExp(query), 'i');
         const users = await User.find({$or: [{fullName: regex}, {username: regex}]}, ('username photoPath fullName'))
             .limit(SEARCH_INFO_PER_PAGE)
             .skip(skip);
