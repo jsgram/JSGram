5be66c90f7782b8eae34ff81f400ee528c581627
vglfr <vf.velt@gmail.com>
2019-09-25 17:03:39 +0300

    Implement email notification spam

---
+++ server/src/controllers/event.controllers/send.news.email.ts
@@ -0,0 +1,34 @@
+import { User, IUserModel } from '../../models/user.model';
+import { sendEmailHelper } from '../../helpers/send.email.helper';
+
+import { Request, Response, NextFunction } from 'express';
+import pug from 'pug';
+import path from 'path';
+
+export const sendNewsEmail = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const templatePath = path.join(process.env.HEROKU_ROOT, process.env.TEMPLATE_DIR, 'news.email.pug');
+        const renderTemplate = pug.compileFile(templatePath);
+
+        const users = await User.find({ 'subscriptions.isNewsEmail': true });
+
+        await Promise.all(users.map(async (user: IUserModel) => {
+            const { body: { payload: { compare, sender: { login, html_url } } } }: Request = req;
+            const emailBody = renderTemplate({
+                user,
+                baseUrl: process.env.BACK_PATH,
+                event: {
+                    login,
+                    html_url,
+                    compare,
+                },
+            });
+
+            return await sendEmailHelper(user, emailBody);
+        }));
+
+        res.sendStatus(200);
+    } catch (e) {
+        next(e);
+    }
+};
