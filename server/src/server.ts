153720434f29b1b994c7eafc3c395b1fd8e86a79
vglfr <vf.velt@gmail.com>
2019-09-25 17:03:39 +0300

    Implement email notification spam

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,9 @@
 import { app } from './app';
 import connect from './connect';
+import { notificationLoop } from './worker';
 
 app.listen(process.env.PORT, () => console.info('Listening...'));
-(async (): Promise<void> => await connect(process.env.DB_PATH))();
+(async (): Promise<void> => {
+    await connect(process.env.DB_PATH);
+    await notificationLoop();
+})();
