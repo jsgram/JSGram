b5f3406bf7e4432590e162e5221974e49a06009b
didostap <dribnucko@gmail.com>
2019-10-03 01:02:22 +0300

    feature comments/likes notifications

--- client/src/store/notifications/actions.ts
+++ client/src/store/notifications/actions.ts
@@ -1,6 +1,7 @@
 import { Dispatch } from 'redux';
 import { showAlert } from '../alert/actions';
 import { SocketAPI } from '../../helpers/socket.connection';
+import { ADD_NEW_NOTIFICATION } from './notificationsConfig';
 
 const socketNotifications = new SocketAPI('notifications');
 
@@ -16,7 +17,7 @@ export const joinRoomNotificationSocket = (loggedId: string): (dispatch: Dispatc
 export const emitNewNotificationSocket = (userId: string, username: string, message: string):
     (dispatch: Dispatch) => Promise<void> => async (dispatch: Dispatch): Promise<void> => {
         try {
-            await socketNotifications.emit('add new notification', {userId, username, message});
+            await socketNotifications.emit(ADD_NEW_NOTIFICATION, {userId, username, message});
         } catch (e) {
             console.error(e);
         }
@@ -28,7 +29,7 @@ export const onNewNotificationSocket = (): (dispatch: Dispatch) => Promise<void>
             const fun = (({username, message}: {username: string, message: string}): void => {
                 dispatch(showAlert(`${username} ${message}`, 'success'));
             });
-            await socketNotifications.on('add new notification', fun);
+            await socketNotifications.on(ADD_NEW_NOTIFICATION, fun);
         } catch (e) {
             console.error(e);
         }
--- client/src/store/notifications/notificationsConfig.ts
+++ client/src/store/notifications/notificationsConfig.ts
@@ -1,3 +1,5 @@
 export const LIKE_NOTIFICATION = 'liked your post';
 export const COMMENT_NOTIFICATION = 'commented your post';
 export const FOLLOW_NOTIFICATION = 'started following your post';
+
+export const ADD_NEW_NOTIFICATION = 'add new notification';
