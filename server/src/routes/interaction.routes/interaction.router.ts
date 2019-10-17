6c14eda6281c32394d6a44a41e80cce00bb12a8a
vglfr <vf.velt@gmail.com>
2019-10-17 19:09:28 +0300

    Tinyfix

--- server/src/routes/interaction.routes/interaction.router.ts
+++ server/src/routes/interaction.routes/interaction.router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 
 import { createInteraction } from '../../controllers/interaction.controllers/create.interaction';
+import { isAuthorized } from '../../controllers/auth.controllers/auth.isAuthorized';
 
 export const interactionRouter = Router();
 
-interactionRouter.post('/', createInteraction);
+interactionRouter.post('/', isAuthorized, createInteraction);
